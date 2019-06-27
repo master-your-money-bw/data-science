0fd96674126aa9b989d622ab21113c428aa2bf39
PWalis <pwilky1016@gmail.com>
2019-06-27 08:33:56 -0700

    updated dataset

--- Patrick/finaldata.py
+++ Patrick/finaldata.py
@@ -16,7 +16,7 @@ data.rename(columns={'FINCBTAX':'Income before tax'}, inplace=True)
 data.head()
 
 #%%
-data['Income minus total expenses'] = ((data['FINATXEM']/4) - data['Total expenses'])
+data['Income minus total expenses'] = ((data['Income After Taxes']/4) - data['Total expenses'])
 data.head()
 
 #%%
@@ -36,3 +36,38 @@ data['possible savings'].describe()
 
 #%%
 data.to_csv(path_or_buf='C:/Users/Patrick/Desktop/Repos/data-science/datasets/almost_final.csv')
+
+#%%
+"""Remove NaN and other unwanted data and load new dataset file"""
+data = pd.read_csv('../datasets/almost_final.csv')
+data.head()
+
+#%%
+data.rename(columns={'FINATXEM':'Income After Tax'}, inplace=True)
+data.head()
+
+
+#%%
+data['Necessities'] = (data['Total Transportation cost'] + data['Total Housing'] +
+                      data['Total Food'])
+data.head()
+#%%
+data['50%'] = data['Necessities']/(data['Income After Taxes']/4) 
+data.head(20)
+
+#%%
+data['30%'] = data['Luxury expenses']/(data['Income After Taxes']/4)
+data.head()
+
+#%%
+data['20%'] = (data['possible savings']*3) / (data['Income After Taxes']/4)
+data.head(20)
+
+#%%
+data['50%_delta'] = data['50%']-.5
+data['30%_delta'] = data['30%']-.3
+data['20%_delta'] = data['20%']-.2
+data.head()
+
+#%%
+data['target']
